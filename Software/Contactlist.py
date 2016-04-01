b1a6b34178b87778fcdf838e0c5eeeeddbf090a0
Hackin7 <zunmun@gmail.com>
2016-04-01 23:05:56 +0800

    Update Contactlist.py

--- Software/Contactlist.py
+++ Software/Contactlist.py
@@ -1,9 +1,4 @@
-list = [('Terence','86181885','67521202','92739288','94591211'),
-('Ang Keng Yang','93372659'),
-('Ang Wei Zhi','67533507','90769356'),
-('Bei Lai','85976289'),
-('Benjamin','94213366'),
-('Brandon Boo','64021196','96625047'),
-('Brandon Lua','91933388'),
-('Chan Tricia','97348899'),
+list = [
+('Benjamin','94213367'),
 ]
+#Not actual phone no
